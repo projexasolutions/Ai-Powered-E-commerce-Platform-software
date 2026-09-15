d1b29b6e65cc1d8660fefb28e0917cd52fa39639
projexasolutions <projexasolutions@gmail.com>
2026-09-16 01:25:01 +0530

    Add product variant repository

---
+++ backend/src/main/java/com/nextgen/store/catalog/ProductVariantRepository.java
@@ -0,0 +1,8 @@
+package com.nextgen.store.catalog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProductVariantRepository extends JpaRepository<ProductVariant,Long>{
+  List<ProductVariant> findByProductIdAndActiveTrueOrderBySizeAscColorAsc(Long productId);
+}
