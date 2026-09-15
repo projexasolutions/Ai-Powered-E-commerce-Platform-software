ccadcd3b4019625961e965f13c6c8951bded6b9b
projexasolutions <projexasolutions@gmail.com>
2026-09-16 02:54:31 +0530

    Integrate admin dashboard route

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
 import App from './App';
+import AdminDashboard from './AdminDashboard';
+
+const isAdminRoute = window.location.pathname === '/admin';
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    {isAdminRoute ? <AdminDashboard onBack={() => { window.location.href = '/'; }} /> : <App />}
+  </React.StrictMode>
 );
