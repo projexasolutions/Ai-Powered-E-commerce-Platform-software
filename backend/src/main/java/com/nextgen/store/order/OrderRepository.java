8dee3b7817bd7d8b1d482d3b1e300e73ecb4fb24
projexasolutions <projexasolutions@gmail.com>
2026-09-16 02:07:53 +0530

    Add order repository

---
+++ backend/src/main/java/com/nextgen/store/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.nextgen.store.order;
+
+import org.springframework.data.jpa.repository.*;
+import java.util.*;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+  List<Order> findByUserIdOrderByCreatedAtDesc(Long userId);
+  Optional<Order> findByIdAndUserId(Long id,Long userId);
+}
